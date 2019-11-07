2813a7d27262290bf0b32fbe3ac55013e0109f22
Marko Cubic <markocubic@hotmail.com>
2019-11-07 02:03:46 +0100

    added vjezba2 in Programiranje u Javi, deleted moj_client.py

---
+++ Programiranje u Javi/tude_vjezbe/zvone_vjezba2/test/Vjezba2/myCRC32NGTest.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vjezba2;
+
+import static org.testng.Assert.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import java.util.zip.CRC32;
+import java.util.zip.Checksum;
+
+/**
+ *
+ * @author zvone
+ */
+public class myCRC32NGTest {
+    
+    public myCRC32NGTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @BeforeMethod
+    public void setUpMethod() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDownMethod() throws Exception {
+    }
+
+    /**
+     * Test of update method, of class myCRC32.
+     */
+    @Test
+    public void testUpdate_int() {
+        System.out.println("update");
+        int b = 0;
+        myCRC32 instance = new myCRC32();
+        instance.update(b);
+        
+        CRC32 org = new CRC32();
+        org.update(b);
+        
+        assertEquals(instance.getValue(), org.getValue());
+    }
+
+    /**
+     * Test of update method, of class myCRC32.
+     */
+    @Test
+    public void testUpdate_3args() {
+        System.out.println("update");
+        String str = "Generate CRC32 Checksum For Byte Array Example";
+        byte bytes[] = str.getBytes();
+        
+        myChecksum checksum = new myCRC32();
+        myCRC32 instance = new myCRC32();
+        instance.update(bytes, 0, bytes.length);
+        
+        Checksum checksum2 = new CRC32();
+        CRC32 org = new CRC32();
+        org.update(bytes, 0, bytes.length);
+        
+        assertEquals(instance.getValue(), org.getValue());
+    }
+
+    /**
+     * Test of update method, of class myCRC32.
+     */
+    
+    @Test
+    public void testUpdate_byteArr() { 
+        System.out.println("update");
+        String str = "Generate CRC32 Checksum For Byte Array Example";
+        byte b[] = str.getBytes();
+        myCRC32 instance = new myCRC32();
+        instance.update(b);
+        
+        CRC32 org = new CRC32();
+        org.update(b);
+        
+        assertEquals(instance.getValue(), org.getValue());
+    }
+
+    /**
+     * Test of reset method, of class myCRC32.
+     */
+    
+    @Test
+    public void testReset() {
+        System.out.println("reset");
+        myCRC32 instance = new myCRC32();
+        instance.reset();
+        
+        CRC32 org = new CRC32();
+        org.reset();
+        
+        assertEquals(instance.getValue(), org.getValue());
+    }
+
+    /**
+     * Test of getValue method, of class myCRC32.
+     */
+    
+    @Test
+    public void testGetValue() {
+        System.out.println("getValue");
+        myCRC32 instance = new myCRC32();
+        long expResult = 0L;
+        long result = instance.getValue();
+        
+        assertEquals(result, expResult);
+    }
+
+    
+}
---
+++ Programiranje u Javi/vjezba2/src/main/java/com/mycompany/vjezba2/myCRC32.java
@@ -0,0 +1,86 @@
+
+package com.mycompany.vjezba2;
+
+public class myCRC32 implements myChecksum {
+    private int crc;
+    
+    public myCRC32() {
+    }
+    
+    private static int[] crc_table = make_crc_table();
+    
+    private static int[] make_crc_table () {
+        int[] crc_table = new int[256];
+        for (int n = 0; n < 256; n++){
+            int c = n;
+            for(int k = 8;  --k >= 0;) {
+                if ((c & 1) != 0)
+                    c = 0xedb88320 ^ (c >>> 1);
+                else
+                    c = c >>> 1;
+            }
+            crc_table[n] = c;
+        }
+        return crc_table;
+    }
+
+    public void update (int bval) {
+        int c = ~crc;
+        c = crc_table[(c ^ bval) & 0xff] ^ (c >>> 8);
+        crc = ~c;
+    }
+    
+    public void update(byte[] b, int off, int len) {
+        if (b == null) {
+            throw new NullPointerException();
+        }
+        if (off < 0 || len < 0 || off > b.length - len) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+            crc  = 0xFFFFFFFF;       // initial contents of LFBSR
+        int poly = 0xEDB88320;   // reverse polynomial
+
+        for (byte a : b) {
+            int temp = (crc ^ a) & 0xff;
+
+            for (int i = 0; i < 8; i++) {
+                if ((temp & 1) == 1) temp = (temp >>> 1) ^ poly;
+                else                 temp = (temp >>> 1);
+            }
+            crc = (crc >>> 8) ^ temp;
+        }
+
+        crc = crc ^ 0xffffffff;
+    }
+    
+    private int updateBytes(int crc, byte[] b, int off, int len){
+
+            crc  = 0xFFFFFFFF;       // initial contents of LFBSR
+            int poly = 0xEDB88320;   // reverse polynomial
+
+            for (byte a : b) {
+                int temp = (crc ^ a) & 0xff;
+
+                for (int i = 0; i < 8; i++) {
+                    if ((temp & 1) == 1) temp = (temp >>> 1) ^ poly;
+                    else                 temp = (temp >>> 1);
+                }
+                crc = (crc >>> 8) ^ temp;
+            }
+
+            crc = crc ^ 0xffffffff;
+            return crc;
+    }
+    
+    public void update(byte[] b) {
+        crc = updateBytes(crc, b, 0, b.length);
+    }
+
+    public void reset() {
+        crc = 0;
+    }
+
+    public long getValue() {
+        return (long)crc & 0xffffffffL;
+    }
+}
