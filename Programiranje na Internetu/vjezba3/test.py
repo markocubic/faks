b9c0df6257f6d4d0f1d1ffdbc89ba481f87c6234
Marko Cubic <markocubic@hotmail.com>
2019-11-13 22:01:18 +0100

    deleted test files

--- Programiranje na Internetu/vjezba3/test.py
+++
@@ -1,4 +0,0 @@
-#!"C:\Users\Marko Ćubić\AppData\Local\Programs\Python\Python38-32\python.exe"
-import cgi
-print ("Content-Type: text/html \n\n")
-print ("Hello, I am from Python using Xampp Server")
